8f8267341fcc0262a99cd8188311cb04e768b0f2
Felipe Alvarez Ortiz <me@felipealvarez.dev>
2021-10-05 20:32:24 -0300

    - Se incorpora la API que recibe y valida imagenes de los grupos

--- Packing.Mvc/Controllers/Productos/GruposController.cs
+++ Packing.Mvc/Controllers/Productos/GruposController.cs
@@ -1,9 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Net.Http.Headers;
 using Packing.Mvc.Data;
 using Packing.Mvc.Models.Dtos;
 using Packing.Mvc.Models.Empresas;
@@ -28,20 +33,28 @@ namespace Packing.Mvc.Controllers.Productos
         }
 
         [HttpPost("GuardarGrupo"), Authorize(Roles = "Administrador")]
-        public async Task<ActionResult> GuardarGrupo(NuevoGrupo request)
+        public async Task<ActionResult> GuardarGrupo([FromForm] string nombreGrupo, IFormFile archivo, CancellationToken cancellationToken)
         {
-            var nuevoNombre = request.NombreGrupo;
-            if (await ExisteGrupoPorNombre(nuevoNombre))
+            if (await ExisteGrupoPorNombre(nombreGrupo))
             {
                 return BadRequest("El grupo ya existe");
             }
 
+            if (archivo.Length <= 0) return BadRequest("El archivo no puede estar vacio");
+            
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/productos/", archivo.FileName);
+
+            await using (var stream = System.IO.File.Create(filePath))
+            {
+                await archivo.CopyToAsync(stream,cancellationToken);
+            }
             await _context.Grupos.AddAsync(new GrupoProducto()
             {
-                NombreGrupo = nuevoNombre,
-                Imagen = null
-            });
-            return await _context.SaveChangesAsync() > 0
+                NombreGrupo = nombreGrupo,
+                Imagen = archivo.FileName
+            },cancellationToken);
+
+            return await _context.SaveChangesAsync(cancellationToken) > 0
                 ? Ok()
                 : BadRequest("Ha ocurrido un error interno, intentelo más tarde o contacte al dev.");
         }
@@ -52,5 +65,6 @@ namespace Packing.Mvc.Controllers.Productos
             if (gruposExistentes.Count <= 0) return false;
             return await _context.Grupos.Where(grupo => grupo.NombreGrupo.Equals(nombreGrupo)).FirstOrDefaultAsync() is not null;
         }
+        
     }
 }
